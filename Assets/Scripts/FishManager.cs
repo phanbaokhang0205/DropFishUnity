b24b64544b3332a77fd56c785a73245eb029c410
Khang Phan <phanbaokhang0205@gmail.com>
2025-03-18 16:27:00 +0700

    init

---
+++ Assets/Scripts/FishManager.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FishManager : MonoBehaviour
+{
+    public static FishManager Instance { get; private set; }
+
+    public GameObject[] fishPrefabs;
+    public List<GameObject> droppedFishes;
+    public Rigidbody2D fishRb = null;
+    public GameObject chosenFish = null;
+    public bool nextFish;
+
+    private string oldTag;
+    private string newTag;
+    private int newTagIndex;
+
+
+    private string[] hexColors = {
+        "#FF0000", // Đỏ
+        "#00FF00", // Xanh lá
+        "#0000FF", // Xanh dương
+        "#FFFF00", // Vàng
+        "#FF00FF", // Hồng
+        "#00FFFF", // Xanh cyan
+        "#FFA500", // Cam
+        "#800080", // Tím
+        "#008000", // Xanh lục đậm
+        "#808080", // Xám
+        "#FFC0CB"  // Hồng nhạt
+    };
+    private int colorIndex;
+    private SpriteRenderer spriteRenderer;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        droppedFishes = new List<GameObject>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //nextFish = false;
+    }
+
+    private void Update()
+    {
+        //nextFish = !chosenFish;
+    }
+
+    public void CreateFish(Vector3 spawnPosition)
+    {
+        if (fishPrefabs.Length == 0) return;
+
+        int randomIndex = Random.Range(0, 4);
+        GameObject newFish = Instantiate(fishPrefabs[randomIndex], spawnPosition, Quaternion.identity);
+
+        chosenFish = newFish;
+        fishRb = chosenFish.GetComponent<Rigidbody2D>();
+
+        fishRb.gravityScale = 0;
+        fishRb.constraints |= RigidbodyConstraints2D.FreezeRotation;
+
+        newFish.transform.parent = this.transform;
+    }
+
+    public void addToDroppedFishes()
+    {
+        fishRb.gravityScale = 1;
+        fishRb.constraints &= ~RigidbodyConstraints2D.FreezeRotation;
+        droppedFishes.Add(chosenFish);
+
+        chosenFish = null;
+        fishRb = null;
+    }
+
+    public void movingFish(Vector3 touchPosition)
+    {
+        chosenFish.transform.position = touchPosition;
+        fishRb.gravityScale = 0;
+        fishRb.constraints |= RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    public void MergeFish(GameObject collision1, GameObject collision2)
+    {
+        if (collision1.tag == collision2.tag)
+        {
+            //fishManager.MergeFish(gameObject, collision.gameObject);
+            if (collision1.transform.position.y > collision2.transform.position.y)
+            {
+                Destroy(collision1);
+                droppedFishes.Remove(collision1);
+
+
+                collision2.transform.localScale *= 1.2f;
+                SplitTag(collision2.tag, collision2);
+                ChangeColor(collision2);
+            }
+        }
+
+    }
+
+    void SplitTag(string tag, GameObject newFish)
+    {
+        oldTag = tag;
+        string[] parts = oldTag.Split('_'); // Tách chuỗi thành ["fish", "1"]
+
+        if (parts.Length == 2) // Kiểm tra có đúng định dạng "fish_x"
+        {
+            newTagIndex = int.Parse(parts[1]); // Lấy số
+            newTagIndex++; // Tăng số lên 1
+            newTag = parts[0] + "_" + newTagIndex; // Ghép lại thành "fish_2"
+
+            newFish.tag = newTag;
+        }
+    }
+    public void ChangeColor(GameObject newFish)
+    {
+        Color newColor;
+        colorIndex = newTagIndex; // Tăng màu theo vòng lặp
+        Debug.Log(colorIndex);
+
+        if (ColorUtility.TryParseHtmlString(hexColors[colorIndex-1], out newColor))
+        {
+            newFish.GetComponent<SpriteRenderer>().color = newColor;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "water")
+        {
+            Debug.Log("Water");
+        }
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Collections;
+using UnityEngine;
+/**
+ * 1. Tại sao phải gán giá trị trong start mà không gán trực tiếp ngoài ?
+ * 2. Tại sao Init Fish không dùng touchPosition mà phải tạo 1 Vector3 mới là spamwnPosition ?
+ * 3. Tại sao điền tham số trong vector3 là 0 rồi mà vẫn gán lại cho spawnPosition.z = 0
+ * 
+ */
+public class PlayerController : MonoBehaviour
+{
+    private FishManager fishManager;
+    private Vector3 touchPosition;
+    private Touch touch;
+
+    private int heightDrop;
+    private int widthDrop;
+
+
+    private void Start()
+    {
+        fishManager = FishManager.Instance;
+
+        heightDrop = Screen.height / 2;
+        widthDrop = Screen.width / 4;
+
+
+        fishManager.CreateFish(touchPosition);
+    }
+
+    void Update()
+    {
+        //Debug.Log(fishManager.nextFish);
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, heightDrop, 10));
+
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (fishManager.chosenFish) return;
+
+
+                //fishManager.CreateFish(touchPosition);
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                if (fishManager.fishRb == null) return;
+
+                fishManager.movingFish(touchPosition);
+
+                checkDragPosition(touch);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                if (fishManager.fishRb == null) return;
+
+                //Debug.Log(fishManager.nextFish + fishManager.chosenFish.tag);
+                fishManager.addToDroppedFishes();
+
+                //if (fishManager.nextFish)
+                //{
+                touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, heightDrop, 10));
+                fishManager.CreateFish(touchPosition);
+
+                //}
+            }
+
+        }
+    }
+
+    void checkDragPosition(Touch touch)
+    {
+        Vector3 newPosition = fishManager.chosenFish.transform.position;
+
+        if (touch.position.x <= widthDrop)
+        {
+            newPosition.x = Camera.main.ScreenToWorldPoint(new Vector3(widthDrop, touch.position.y, 10)).x;
+            fishManager.chosenFish.transform.position = newPosition;
+        }
+        else if (touch.position.x >= Screen.width - widthDrop)
+        {
+            newPosition.x = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width - widthDrop, touch.position.y, 10)).x;
+            fishManager.chosenFish.transform.position = newPosition;
+        }
+    }
+
+}
